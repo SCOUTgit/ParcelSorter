c14136d08f1ce2ade8df7b02584e25e082636567
SCOUTgit <k01095232802@gmail.com>
2019-10-27 10:48:10 +0900

    [Fix] 일부 기종에서 레터박스에 쓰레기값이 나오는 오류 해결

--- Assets/Scripts/CameraResoultion.cs
+++ Assets/Scripts/CameraResoultion.cs
@@ -12,7 +12,6 @@ public class CameraResoultion : MonoBehaviour
         Screen.sleepTimeout = SleepTimeout.NeverSleep;
 
         var camera = gameObject.GetComponent<Camera>();
-         camera.aspect = (float)targetWidthAspect / targetHeightAspect;
 
         float widthRatio = (float)Screen.width / targetWidthAspect;
         float heightRatio = (float)Screen.height / targetHeightAspect;
@@ -21,56 +20,16 @@ public class CameraResoultion : MonoBehaviour
         float heightAdd = 0;
 
         if (widthRatio > heightRatio)
-            widthAdd = heightRatio / (widthRatio * 2) - 0.5f;
+            widthAdd = 1 - heightRatio / widthRatio;
         else
-            heightAdd = widthRatio / (heightRatio * 2) - 0.5f;
+            heightAdd = 1 - widthRatio / heightRatio;
 
-        camera.rect = new Rect(camera.rect.x + Mathf.Abs(widthAdd), camera.rect.y + Mathf.Abs(heightAdd), camera.rect.width + (widthAdd * 2), camera.rect.height + (heightAdd * 2));
-    }
+        camera.rect = new Rect(widthAdd / 2, heightAdd / 2, camera.rect.width - widthAdd, camera.rect.height - heightAdd);
 
-#if UNITY_ANDROID
-    static AndroidJavaObject activityInstance;
-    static AndroidJavaObject windowInstance;
-    static AndroidJavaObject viewInstance;
-
-    const int SYSTEM_UI_FLAG_HIDE_NAVIGATION = 2;
-    const int SYSTEM_UI_FLAG_LAYOUT_STABLE = 256;
-    const int SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION = 512;
-    const int SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN = 1024;
-    const int SYSTEM_UI_FLAG_IMMERSIVE = 2048;
-    const int SYSTEM_UI_FLAG_IMMERSIVE_STICKY = 4096;
-    const int SYSTEM_UI_FLAG_FULLSCREEN = 4;
-
-    public delegate void RunPtr();
-
-    public static void Run()
-    {
-        if (viewInstance != null)
-        {
-            viewInstance.Call("setSystemUiVisibility",
-            SYSTEM_UI_FLAG_LAYOUT_STABLE
-            | SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-            | SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-            | SYSTEM_UI_FLAG_HIDE_NAVIGATION
-            | SYSTEM_UI_FLAG_FULLSCREEN
-            | SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-        }
-
-    }
-#endif
-    public static void DisableNavUI()
-    {
-        if (Application.platform != RuntimePlatform.Android)
-            return;
-        using (AndroidJavaClass unityPlayerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
-        {
-            activityInstance = unityPlayerClass.GetStatic<AndroidJavaObject>("currentActivity");
-            windowInstance = activityInstance.Call<AndroidJavaObject>("getWindow");
-            viewInstance = windowInstance.Call<AndroidJavaObject>("getDecorView");
-
-            AndroidJavaRunnable RunThis;
-            RunThis = new AndroidJavaRunnable(new RunPtr(Run));
-            activityInstance.Call("runOnUiThread", RunThis);
-        }
+        var backgroundCamera = new GameObject("BackgroundCam", typeof(Camera)).GetComponent<Camera>();
+        backgroundCamera.depth = int.MinValue;
+        backgroundCamera.clearFlags = CameraClearFlags.SolidColor;
+        backgroundCamera.backgroundColor = Color.black;
+        backgroundCamera.cullingMask = 0;
     }
 }
